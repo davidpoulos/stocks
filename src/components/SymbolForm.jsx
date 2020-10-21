5d15e80bacab5d761f5cedb98b1e69b88d1b8d22
David Poulos <davidstephenpoulos@gmail.com>
2020-10-20 19:30:30 -0500

    adding eslint file

---
+++ src/components/SymbolForm.jsx
@@ -0,0 +1,16 @@
+import React from React;
+
+/**
+ * 
+ */
+function StockForm() {
+    return (
+        <div>
+            <form>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+export default StockForm
